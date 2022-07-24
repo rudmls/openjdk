8e4349f4bb6e043e46e74f7ba341d6130c36cf43
Ruddy Monlouis <r.monlouis@gmail.com>
2022-07-25 00:07:04 +0200

    update

--- Makefile
+++ Makefile
@@ -4,3 +4,17 @@ hadolint:
 	--workdir=/work \
 	hadolint/hadolint hadolint --config ./.hadolint.yml $(shell find ./ -name Dockerfile | sed 's/^./\/work/g' | tr '\n' ' ')
 
+login:
+	echo "$(DOCKER_PASSWORD)" | docker login \
+	--username $(DOCKER_USERNAME) \
+	--password-stdin
+
+my_test:
+	ifdef toto
+			@echo $(toto)
+	else
+			@echo 'no toto around'
+	endif
+
+build:
+	docker build --tag $(DOCKER_USERNAME)/hadoop-base:$(current_branch) ./base
--- render.sh
+++ render.sh
@@ -2,16 +2,17 @@
 #!/bin/bash
 
 function render() {
-  sed -r "s|\{\{\s*java_version\s*\}\}|${1}|" ${2}
+    sed -r "s|\{\{\s*java_version\s*\}\}|${1}|" ${2}
 }
 
 function main() {
-  versions=(8 11 17)
-  mkdir -p dockerfiles
-  for version in ${versions[*]}; do
-    mkdir -p dockerfiles/openjdk${version}
-    render "${version}" "Dockerfile.template" > ./dockerfiles/openjdk${version}/Dockerfile
-  done
+    versions=(8 11 17)
+    mkdir -p dockerfiles
+    for version in ${versions[*]}; do
+        mkdir -p dockerfiles/openjdk${version} && \
+        cp ./assets ./dockerfiles/openjdk${version} && \
+        render "${version}" "Dockerfile.template" > ./dockerfiles/openjdk${version}/Dockerfile || exit 1
+    done
 }
 
 main
