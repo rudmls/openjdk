89f53470caf479a2d64bab05f27faaccec2c8825
Ruddy Monlouis <r.monlouis@gmail.com>
2022-07-24 21:04:27 +0200

    first commit

---
+++ render.sh
@@ -0,0 +1,17 @@
+
+#!/bin/bash
+
+function render() {
+  sed -r "s|\{\{\s*java_version\s*\}\}|${1}|" ${2}
+}
+
+function main() {
+  versions=(8 11 17)
+  mkdir -p dockerfiles
+  for version in ${versions[*]}; do
+    mkdir -p dockerfiles/openjdk${version}
+    render "${version}" "Dockerfile.template" > dockerfiles/openjdk-${version}/Dockerfile
+  done
+}
+
+main
